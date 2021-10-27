cc937e421901ea42d256f0cb6ebe462ba790890d
Sylwia <sylwia@valointranet.com>
2021-10-27 12:39:32 +0200

    awesome leap

--- src/model/Enemy.ts
+++ src/model/Enemy.ts
@@ -1,18 +1,23 @@
 import { Constants } from "../utils/Constants";
 import { PlayerAnimationHelper } from "../utils/PlayerAnimationHelper";
+import { Ground } from "./Ground";
 import { Player } from "./Player";
 
 export class Enemy extends Player {
     constructor() {
         super();
         this.rideAnimation = new PlayerAnimationHelper([
-            "enemy.png"
+            "zombie1frame1.png",
+            "zombie1frame2.png",
+            "zombie1frame3.png",
+            "zombie1frame4.png"
         ]);
         this.spriteSize = Constants.enemySize;
+        this.yPosition = Constants.groundLevel + 10;
+
     }
 
     public update() {
-        if (Constants.isGameRunning)
-            this.xPosition = (((this.xPosition - Constants.scrollSpeed * 1.5) % Constants.gameCanvasWidth) + Constants.gameCanvasWidth) % Constants.gameCanvasWidth;
+        this.xPosition = (((this.xPosition - Constants.scrollSpeed * 1.5) % Constants.gameCanvasWidth) + Constants.gameCanvasWidth) % Constants.gameCanvasWidth;
     }
 }
