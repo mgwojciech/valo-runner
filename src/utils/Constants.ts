56ec14ae0c52840b9169a2361d17b8c77e1cd71c
Sylwia <sylwia@valointranet.com>
2021-10-27 13:15:55 +0200

    enemy grew bigger

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,7 +2,7 @@ export class Constants {
     public static groundLevel = 600;
     public static jumpTime = 10;
     public static playerSize = 150;
-    public static enemySize = 150;
+    public static enemySize = 200;
     public static scrollSpeed = 4;
     public static gravityModified = 0.015;
     public static gameCanvasWidth = 1200;
